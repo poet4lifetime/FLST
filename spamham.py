900d4b84346eef878bf6bf33e54ca9b42ca8a5c7
poet4lifetime <poet4lifetime@gmail.com>
2014-12-07 15:26:21 +0100

    read in files

--- spamham.py
+++ spamham.py
@@ -1,19 +1,21 @@
 __author__ = 'nicky'
 
+
 vocabularyFile = open('vocab_100000.wl', encoding = 'latin1')
 vocabulary = []
-
-spamTrainingFile = open('spam_training', encoding = 'latin1')
-spamTrainingData = {}
-
-hamTrainingFile = open('ham_training', encoding = 'latin1')
-hamTrainingData = {}
 
 # creates list of vocabulary
 for line in vocabularyFile:
     item = line.strip('\n')
     vocabulary.append(item)
 
+vocabularyFile.close()
+
+
+spamTrainingFile = open('spam_training', encoding = 'latin1')
+spamTrainingData = {}
+
+# creates dict of spam training files and freq as key
 for line in spamTrainingFile:
     item = line.strip('\n')
     if '#*#*#' in item:
@@ -23,6 +25,13 @@ for line in spamTrainingFile:
     else:
         spamTrainingData[item] += 1
 
+spamTrainingFile.close()
+
+
+hamTrainingFile = open('ham_training', encoding = 'latin1')
+hamTrainingData = {}
+
+# creates dict of ham training files and freq as key
 for line in hamTrainingFile:
     item = line.strip('\n')
     if '#*#*#' in item:
@@ -31,3 +40,6 @@ for line in hamTrainingFile:
         hamTrainingData[item] = 1
     else:
         hamTrainingData[item] += 1
+
+hamTrainingFile.close()
+
