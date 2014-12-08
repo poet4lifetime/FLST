acec764f5c81fb83c0e80b2362e955b5994a9e88
poet4lifetime <poet4lifetime@gmail.com>
2014-12-08 15:16:40 +0100

    feature extraction, counts, smoothed probability (has to be checked)

--- spamham.py
+++ spamham.py
@@ -1,79 +1,98 @@
 __author__ = 'nicky'
 
-#_____________________________Function definitions_____________________________
-'''
-# feature extraction: filters out words that are not in the vocabulary
-def filterOut(dataBase, comparisonVocabulary):
-    newBase = {}
-    for element in dataBase:
-        if dataBase[element] in comparisonVocabulary:
-            newBase[element] = dataBase[element]
-    return newBase
-'''
-#_____________________________Reading in files_____________________________
+##########################################################################
+#--------------------------- Reading in files ---------------------------#
+#----------------------- also: feature extraction -----------------------#
+##########################################################################
 
 vocabularyFile = open('vocab_100000.wl', encoding = 'latin1')
-vocabulary = []
-# creates list of vocabulary
+vocabulary = {}
+# creates a dictionary out of vocabulary with default value 0
 for line in vocabularyFile:
     item = line.strip('\n')
-    vocabulary.append(item)
+    vocabulary[item] = 0
 vocabularyFile.close()
 
 spamTrainingFile = open('spam_training', encoding = 'latin1')
 spamTrainingData = {}
-# creates dict of spam training files and number of occurrences as key
+# creates dict of spam training files !occurring in the vocabulary! and number of occurrences as key
 for line in spamTrainingFile:
     item = line.strip('\n')
-    if '#*#*#' in item:
-        pass
-    if item not in spamTrainingData.keys():
-        spamTrainingData[item] = 1
-    else:
-        spamTrainingData[item] += 1
+    if item in vocabulary.keys():
+        if '#*#*#' in item:
+            pass
+        if item not in spamTrainingData.keys():
+            spamTrainingData[item] = 1
+        else:
+            spamTrainingData[item] += 1
 spamTrainingFile.close()
 
 hamTrainingFile = open('ham_training', encoding = 'latin1')
 hamTrainingData = {}
-# creates dict of ham training files and number of occurrences as key
+# creates dict of ham training files !occurring in the vocabulary! and number of occurrences as key
 for line in hamTrainingFile:
     item = line.strip('\n')
-    if item in vocabulary:
+    if item in vocabulary.keys():
         if item not in hamTrainingData.keys():
             hamTrainingData[item] = 1
         else:
             hamTrainingData[item] += 1
 hamTrainingFile.close()
 
-print(len(hamTrainingData))
+##########################################################################
+#-------------------------------- Counts --------------------------------#
+##########################################################################
 
-
-'''
-nPlusHam = 0
-nPlusSpam = 0
-Nspam = 0
-Nham = 0
+# discounting parameter
 d = 0.7
 
-# smoothing
+# number of occurrences of all elements in spam
+sumNSpam = 0
+for element in spamTrainingData:
+    sumNSpam += spamTrainingData[element]
+
+# number of occurrences of all elements in ham
+sumNHam = 0
+for element in hamTrainingData:
+    sumNHam += hamTrainingData[element]
+
+# number of unique elements in spam
+nPlusSpam = len(spamTrainingData)
+
+# number of unique elements in ham
+nPlusHam = len(hamTrainingData)
+
+alphaSpam = d * nPlusSpam / sumNSpam
+alphaHam = d * nPlusHam / sumNHam
+
+##########################################################################
+#------------------------ Smoothed probabilities ------------------------#
+##########################################################################
 
 spamTrainingDataProbability = {}
 hamTrainingDataProbability = {}
 
 for item in spamTrainingData:
-    Nspam += spamTrainingData[item]
-    if spamTrainingData[item] > 0:
-        nPlusSpam += 1
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! check whether we need len vocab or len spamTrainData
+    spamTrainingDataProbability[item] = (spamTrainingData[item] - d) / sumNSpam + alphaSpam * (1 / len(vocabulary))
+
+    #check how max() works
+    '''
     probability = max(spamTrainingData[item] - d, 0.0) / Nspam + ((d * nPlusSpam) / (Nspam * (len(spamTrainingData))))
     spamTrainingDataProbability[item] = probability
+    '''
 
 for item in hamTrainingData:
-    Nham += hamTrainingData[item]
-    if hamTrainingData[item] > 0:
-        nPlusHam += 1
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! check whether we need len vocab or len hamTrainData
+    hamTrainingDataProbability[item] = (hamTrainingData[item] - d) / sumNHam + alphaHam * (1 / len(vocabulary))
+
+    #check how max() works
+    '''
     probability = max(hamTrainingData[item] - d, 0.0) / Nham + ((d * nPlusHam) / (Nham * (len(hamTrainingData))))
     hamTrainingDataProbability[item] = probability
+    '''
 
+'''
 classOfItem = {}
 
 for item in vocabulary:
